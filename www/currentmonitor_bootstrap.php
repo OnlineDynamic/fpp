94f57bfcd8dd346b3815da708c6ddedfb6df6dfb
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-06-22 22:11:39 +0100

    addition of jquery-ui-slider-pips

--- www/currentmonitor_bootstrap.php
+++ www/currentmonitor_bootstrap.php
@@ -165,15 +165,19 @@
     </style>
     <script>
         $(function () {
-            $("#slider-range").slider({
-                range: true,
-                min: 0,
-                max: 500,
-                values: [75, 300],
-                slide: function (event, ui) {
-                    $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-                }
-            });
+            $("#slider-range")
+                .slider({
+                    range: true,
+                    min: 0,
+                    max: 500,
+                    values: [75, 300],
+                    slide: function (event, ui) {
+                        $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+                    }
+                })
+                .slider("pips")
+                .slider("float");
+
             $("#amount").val("$" + $("#slider-range").slider("values", 0) +
                 " - $" + $("#slider-range").slider("values", 1));
         });
