278d543886e05227fb50f2a31541a7736c82fc8b
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-07-14 17:38:22 +0100

    example set max pips to 2000

--- www/currentmonitor_bootstrap.php
+++ www/currentmonitor_bootstrap.php
@@ -169,7 +169,7 @@
                 .slider({
                     range: true,
                     min: 0,
-                    max: 500,
+                    max: 2000,
                     values: [75, 300],
                     slide: function (event, ui) {
                         $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
