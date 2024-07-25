cbe65ee943c8268013de680ba1b449cc3f8329f1
OnlineDynamic <stuart_ledingham@hotmail.com>
2024-07-25 23:15:44 +0100

    further additions of gpio configs

--- www/api/controllers/gpio.php
+++ www/api/controllers/gpio.php
@@ -1,6 +1,6 @@
 <?
 // GET /api/gpio/config
-
+// API returns information on which gpio is assigned to which function and highlights error states where pin is incorrectly assigned multiple times
 function GPIOConfig()
 {
     global $fppDir;
@@ -84,20 +84,20 @@ function GPIOConfig()
                     $all_gpios[$k]['Input'] = true;
                     $all_gpios[$k]['Function'] = 'Cape Input Interrupt';
                     $all_gpios[$k]['InUse'] = true;
-                    $all_gpios[$k]['Description'] = "Interrupt for: " + $x['chip'];
+                    $all_gpios[$k]['Description'] = "Interrupt for: " . $x['chip'];
                 }
             }
             //detect gpio lines on chips with assignments
             foreach ($all_gpios as $k => $gp) {
                 foreach ($x['actions'] as $a) {
-                    if ($gp['pin'] == ($x['pin'] + '-' + $a['line'])) {
+                    if ($gp['pin'] == ($x['pin'] . '-' . $a['line'])) {
                         if ($gp['InUse'] == true) {
                             $all_gpios[$k]['ConfigError'] = true;
                         }
                         $all_gpios[$k]['Input'] = true;
                         $all_gpios[$k]['Function'] = 'Cape Input Actions';
                         $all_gpios[$k]['InUse'] = true;
-                        $all_gpios[$k]['Description'] = "Action: " + $a['action'];
+                        $all_gpios[$k]['Description'] = "Action: " . $a['action'];
                     }
                 }
             }
@@ -106,26 +106,48 @@ function GPIOConfig()
     }
 
     //Cape - PWM Outputs
-    //TO DO
+    $pwm_files = preg_grep('~\.(json)$~', scandir("$mediaDirectory/tmp/pwm"));
 
-    //Cape - String Outputs
-    $str_files = scandir("$mediaDirectory/tmp/strings");
-    //print_r($str_files);
-    foreach ($str_files as $file) {
-        if (file_exists($file)) {
-            $cape_strings_config_json = file_get_contents($file);
-            $cape_strings_config = json_decode($co_other_config_json, true);
+    foreach ($pwm_files as $file) {
+        if (file_exists("$mediaDirectory/tmp/strings/$file")) {
+            $cape_pwm_config_json = file_get_contents("$mediaDirectory/tmp/pwm/$file");
+            $cape_pwm_config = json_decode($cape_pwm_config_json, true);
 
-            foreach ($cape_strings_config['outputs'] as $x) {
+            foreach ($cape_pwm_config['outputs'] as $x) {
                 foreach ($all_gpios as $k => $gp) {
                     if ($gp['pin'] == $x['pin']) {
                         if ($gp['InUse'] == true) {
                             $all_gpios[$k]['ConfigError'] = true;
                         }
                         $all_gpios[$k]['Output'] = true;
-                        $all_gpios[$k]['Function'] = 'Cape String Output';
+                        $all_gpios[$k]['Function'] = 'Cape PWM Output';
                         $all_gpios[$k]['InUse'] = true;
-                        $all_gpios[$k]['Description'] = 'Cape Pixel Port #';
+                        $all_gpios[$k]['Description'] = 'Cape PWM #';
+                    }
+                }
+            }
+        }
+    }
+
+    //Cape - String Outputs
+    $str_files = preg_grep('~\.(json)$~', scandir("$mediaDirectory/tmp/strings"));
+
+    foreach ($str_files as $file) {
+        if (file_exists("$mediaDirectory/tmp/strings/$file")) {
+            $cape_strings_config_json = file_get_contents("$mediaDirectory/tmp/strings/$file");
+            $cape_strings_config = json_decode($cape_strings_config_json, true);
+            //print_r($cape_strings_config);
+            foreach ($cape_strings_config['outputs'] as $k => $x) {
+
+                foreach ($all_gpios as $z => $gp) {
+                    if ($gp['pin'] == $x['pin']) {
+                        if ($gp['InUse'] == true) {
+                            $all_gpios[$z]['ConfigError'] = true;
+                        }
+                        $all_gpios[$z]['Output'] = true;
+                        $all_gpios[$z]['Function'] = 'Cape String Output';
+                        $all_gpios[$z]['InUse'] = true;
+                        $all_gpios[$z]['Description'] = 'Cape Pixel Port #' . ($k + 1);
                     }
                 }
             }
